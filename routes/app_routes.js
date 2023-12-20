247548e12a90af4ab93dbfc90f6ec6226d0902b5
ShivdasJadhav <jshivdas07@gmail.com>
2023-12-20 18:57:46 +0530

    unwanted functions removed and routes are optimized

--- routes/app_routes.js
+++ routes/app_routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { func_app } = require("../controllers/func_app");
 const { common } = require("../controllers/common");
-router.get("/", common.verifyToken, func_app.getAllArts);
 router.get("/getCount", common.verifyToken, func_app.getCount);
 router.post("/newArt", common.verifyToken, func_app.addArt);
 router.get("/getArtById/:id", common.verifyToken, func_app.getArtById);
